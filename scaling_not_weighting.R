22f2f476584e9515f1a68b66f97ad103015d20bb
mikeroswell <mike.roswell@gmail.com>
2018-12-04 13:51:54 -0500

    Perhaps a simpler graphic representing the scaling argument Jonathan sent in August. So far this feels like the most helpful visualization of the means. However, some problems persist. One of these is it's unclear how to compare the value of the mean across graphs. A second is that I don't yet actually have a general idea of the scaling.

--- scaling_not_weighting.R
+++ scaling_not_weighting.R
@@ -1,25 +1,51 @@
-# I want to play with visualizing the mean as the middle of a distribution, where different values of l correspond to different scalings. 
+# I want to play with visualizing the mean as the middle of a distribution, where different values of l correspond to different scalings of the axis. 
+# future drafts could make the graph look like a see-saw, where the current vertical line is a fulcrum, and bars have units but no y-axis.
 
 #start by taking a very simple distribution like 2,3,5
 library(tidyverse)
 library(vegan)
 
-a<-c(2,3,5)
-a_norm<-a/sum(a)
-a_rare<-1/a_norm
-a_rare
-#unweighted by # individuals
-heights<-rep(1, length(a_rare))
-plot(a_rare,heights, type="h", )
-abline(v=mean(a_rare), col="red")
+#use 20,30,50 so it's obvious why x!=3
+a<-c(20,30,50)
+#This script isn't that flexible yet b/c the x-axis ticks don't work for randomly generated data, for example
+#a<-rpois(6,5)
+a_norm<-a/sum(a) #normalized, rel abundances
+a_rare<-1/a_norm #rarities
+
+#make a d.f. for easy manipulation of scales in ggplot2
+comm<-data.frame("rarity"=a_rare,"abundance"=a)
+
+
+#need to figure out how to set bar widths the same, or better yet replace with boxes or somehting that looks like units (and then go back to 2,3,5 instead of 20,30,50)
+
+#unweighted by # individuals (as far as I know this is not a diversity metric people use, although I've encountered mean and geometric mean rel. abundance)
+comm %>% ggplot(aes(rarity, rep(1, length(a))))+
+    geom_col(width=0.01)+
+    geom_vline(xintercept=mean(a_rare), color="red")+
+    theme_classic()
 
 #weighted by # individuals
-plot(a_rare,a, type="h", ylim=c(0,6) )
-abline(v=1/(mean(a)/sum(a)), col="red")
+
+#Richness (arithmetic scale)
+comm %>% ggplot(aes(rarity, abundance))+
+    geom_col(width=0.01)+
+    geom_vline(xintercept=renyi(comm$abundance, scales=0, hill=T), color="red")+
+    theme_classic()
 
 #rescale x axis
-plot(a_rare,a, type="h", ylim=c(0,6), log="x" )
-abline(v=mean(a_rare), col="red")
+#shannon (log) scale
+comm %>% ggplot(aes(rarity, abundance))+
+    geom_col(width=0.01)+
+    scale_x_continuous(trans="log", limits=c(1.8,6), breaks=c(2,3,4,5))+
+    geom_vline(xintercept=renyi(comm$abundance, scales=1, hill=T), color="red")+
+    theme_classic()
+
+#Simpson (harmonic scale, see transformation (trans) for details)
+comm %>% ggplot(aes(rarity, abundance))+
+    scale_x_continuous(trans=scales::trans_new("recip", function(x){1/(-x)}, inverse=function(x){1/(-x)}))+
+    geom_vline(xintercept=renyi(comm$abundance, scales=2, hill=T), color="red")+
+    geom_col(width=0.01)+
+    theme_classic()
 
 
 
