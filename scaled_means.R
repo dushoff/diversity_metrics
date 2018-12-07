5197b922e75081278c5bdf1d327a77b6a5158b5c
mikeroswell <mike.roswell@gmail.com>
2018-12-06 20:43:31 -0500

    solved the stupid problem with the widths of bars in geom_bar changing with the scale transformation by using geom_segment and making sure the x and xend arguments were inside an explicit aes()

--- scaled_means.R
+++ scaled_means.R
@@ -1,22 +1,28 @@
 library(tidyverse)
-theme_set(theme_bw())
+theme_set(theme_classic())
 
 require(scales) # trans_new() is in the scales library
-
-pfun=function(x, pow){
+#need the reverse bit for the scaling of the axes to get them in the right orientation
+pfun=function(x, pow, rever=F){
 	if (pow==0) return(log(x))
+    if (rever==T){
+    if (pow<0) return(-x^pow)
+        return(x^pow)}
 	return(x^pow)
 }
-ipfun=function(x, pow){
+
+ipfun=function(x, pow, rever=F){
 	if (pow==0) return(exp(x))
+    if(rever==T){if (pow<0) return(-x^(1/pow))
+        return(x^(1/pow))}
 	return(x^(1/pow))
 }
 
 ## This is weird craftiness to make functions above work with coord_trans
 power_trans = function(pow) trans_new(name="power"
-	, transform=function(x) pfun(x, pow)
-	, inverse=function(x) ipfun(x, pow)
-	, breaks = function(x)extended_breaks(n = 30)(x)
+    , transform=function(x) pfun(x, pow, rever=T)
+	, inverse=function(x) ipfun(x, pow, rever=T)
+	#, breaks = function(x)extended_breaks(n = 30)(x)
 )
 
 ## pow does not work if passed, but does work if set globally
@@ -29,11 +35,11 @@ rarity_plot <- function(abundance, p){
 		sum(abundance*pfun(rarity, p))/sum(abundance)
 		, p
 	)) %>% pull(div)
-	rp <- (ggplot(rf, aes(x=names, y=rarity, size=abundance))
-		+ geom_point() + scale_size_area()
-		+ coord_trans(y=power_trans(pow=p))
-		+ scale_y_continuous(breaks=c(1, 2, 5, 10, 20, 30))
-		+ geom_hline(yintercept=div)
+	rp <- (ggplot(rf, aes(x=rarity, y=abundance))
+		+ geom_segment(aes(x=rarity, xend=rarity, y=abundance, yend=0))
+		+ scale_x_continuous(trans=power_trans(pow=p), breaks=c(1,3,5,10,50))
+		#+ scale_size_identity()
+		+ geom_vline(xintercept=div, color="red")
 	)
 	return(rp)
 }
@@ -43,3 +49,7 @@ ab <- c(300,30,10)
 rarity_plot(ab, 1)
 rarity_plot(ab, 0)
 rarity_plot(ab, -1)
+?stat
+
+
+
