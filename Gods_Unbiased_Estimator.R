c7a549cbf75a3fb70bd7d5d947161847e546d128
mikeroswell <mike.roswell@gmail.com>
2018-12-09 17:52:13 -0500

    had a bad formula for chao1 before

--- Gods_Unbiased_Estimator.R
+++ Gods_Unbiased_Estimator.R
@@ -11,18 +11,33 @@ GUE<-function(freqs, true_p,l){
            (sum(freqs*(1/true_p)^l))^(1/l)
     )
 }
+
+
 #Chao1, with her own weird thing for no doubletons
-c1<-function(x){length(x>0)+ifelse(
+c1<-function(x){sum(x>0)+ifelse(
     sum(x==2)>0,
     sum(x==1)^2/(sum(x==2)*2),
     sum(x==1)*(sum(x==1)-1)/2)}
 
+#Good-Turing 1 chao-type estimator
+GT1<-function(x){c1(x)-(sum(x==2))}
+
+#Unsmoothed Good-Turing rarity estimator; problems with infinitiy
+
+# GT2<-function(x){
+#     f_tab<-map_dfr(1:max(x), function(y){return(data.frame(abun=y, f=sum(x==y)))})
+#     sum(!is.na(map_dfr(1:max(x), function(z){
+#     #1/(r+1/sum(n_i)*n_(r+1)/n_r)
+#         return(data.frame(f_tab[z,"f"]*f_tab[z,"abun"]/((z+1)/sum(f_tab$f)*max(f_tab[z+1,"f"],0)/f_tab[z,"f"])))
+#     }))/sum(x))
+# }
+
 #do reps times to test bias, store to a list
 reps<-50
 #number of true species
-S<-50
+S<-75
 #number of individuals in sample
-N<-30
+N<-300
 l<-1
 
 out<-map_dfr(1:reps, function(x){
@@ -41,10 +56,12 @@ true<-GUE(true_p, true_p, l)
 gods<-GUE(freqs/sum(freqs), true_p, l)   
 naive<-GUE((freqs/sum(freqs))[freqs/sum(freqs)!=0],(freqs/sum(freqs))[freqs/sum(freqs)!=0],l)
 chao1<-c1(freqs)
+simple_good1<-GT1(freqs)
+# good_plugin<-GT2(freqs)
 
 
 
-return(data.frame(true, gods, naive, chao1, S, l, N))
+return(data.frame(true, gods, naive, chao1, simple_good1, l, N))
 })
 
 
@@ -60,6 +77,12 @@ plot_ests<-function(df, e1, e2){(df %>% ggplot(aes(x=get(e1), y=get(e2)))
 plot_ests(out, "gods", "chao1")
 plot_ests(out, "gods", "naive")
 plot_ests(out, "chao1", "naive")
+plot_ests(out, "gods", "simple_good1")
+plot_ests(out, "chao1", "simple_good1")
+# plot_ests(out, "gods", "good_plugin")
+# plot_ests(out, "good_plugin", "naive")
+
+
 
 
 
