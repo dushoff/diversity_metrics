70c1945d9b2e52328c07af9a5e7a4b5fee476a48
Jonathan Dushoff <dushoff@mcmaster.ca>
2018-12-11 09:55:56 -0500

    Committing midstream because Mike is here

--- scaled_means.R
+++ scaled_means.R
@@ -5,20 +5,14 @@ library(grid)
 theme_set(theme_tufte(base_family = "sans"))
 
 require(scales) # trans_new() is in the scales library
-#need the reverse bit for the scaling of the axes to get them in the right orientation
-pfun=function(x, pow, rever=F){
+pfun=function(x, pow){
 	if (pow==0) return(log(x))
-    if (rever==T){
-    if (pow<(0)) return(-x^pow)
-        return(x^pow)}
-	return(x^pow)
+	return(sign(pow)*x^pow)
 }
 
-ipfun=function(x, pow, rever=F){
+ipfun=function(x, pow, offset=0){
 	if (pow==0) return(exp(x))
-    if(rever==T){if (pow<(0)) return(-x^(1/pow))
-        return(x^(1/pow))}
-	return(x^(1/pow))
+	return(sign(pow)*(x+offset)^(1/pow))
 }
 
 #add a diversity function to make graphs more complicated
@@ -33,11 +27,10 @@ prettify <- function(breaks){
     return(round(breaks, digits = digits))
 }
 
-
 power_trans = function(pow) trans_new(name="power"
-    , transform = function(x) pfun(x, pow, rever=T)
-	, inverse = function(x) ipfun(x, pow, rever=T)
-	, breaks = function(x) prettify(trans_breaks(function(x) pfun(x, pow, rever=T), function(x) ipfun(x, pow, rever=T), n =5) (c(0,x)*1.1))
+    , transform = function(x) pfun(x, pow)
+	, inverse = function(x) ipfun(x, pow)
+	, breaks = function(x) prettify(trans_breaks(function(x) pfun(x, pow), function(x) ipfun(x, pow), n =5) (c(0,x)*1.1))
     , domain = c(1, 10000)
 )
 
@@ -71,7 +64,7 @@ rarity_plot <- function(abundance, p){
 	  #This makes a nice line that doesn't have issues with scaling
 	# + geom_segment(aes(x=rarity, xend=rarity, y=abundance, yend=0), size=1.6)
 	
-	   # +coord_cartesian(clip="off")
+	   +coord_cartesian(clip="off")
 	
 	#this one seems to work better with the labels
 	+ scale_x_continuous(trans=power_trans(pow=p))
