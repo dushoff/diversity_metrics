b073d04344a692a5ea7fc997b82434ceeaef21b9
mikeroswell <mike.roswell@gmail.com>
2018-12-10 20:30:23 -0500

    giving up on the original geom_barplot and going to generate a more basic plot, probably using ggplot but in hacky ways.
    Also some sparse notes on a little script to write sometime

--- scaled_means.R
+++ scaled_means.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(ggthemes)
+library(cowplot)
 theme_set(theme_tufte(base_family = "sans"))
 
 require(scales) # trans_new() is in the scales library
@@ -50,21 +51,25 @@ rarity_plot <- function(abundance, p){
 		sum(abundance*pfun(rarity, p))/sum(abundance)
 		, p
 	)) %>% pull(div)
-	rf2<-data.frame("boxcol"=sample(c("a", "b", "c","d","e","f","g","h","i","j", "k", "l"), nrow(fancy_rep(rf)), replace=T),"gr"=as.factor(1:nrow(fancy_rep(rf))),fancy_rep(rf))
+	#I don't think all those letters are necessary at this point. 
+	rf2<-data.frame("gr"=as.factor(1:nrow(fancy_rep(rf))),fancy_rep(rf))
 	rp <- (ggplot(rf2, aes(x=rarity))
-	             +geom_bar(aes(group=gr),fill="grey", colour="black", width=1.1^p/10*10^p,size=0.1)
-	       # +scale_fill_manual(c("red", "blue"))
-	       + geom_vline(xintercept=div, color="red", size=1.5)
-	       + geom_vline(xintercept=dfun(ab,1), color="blue", linetype=3, size=1.1)
-	       + geom_vline(xintercept=dfun(ab,-1), color="purple", linetype=4, size=1.1)
-	       + geom_vline(xintercept=dfun(ab,0), color="green", linetype=5, size=1.1)
+	       #This makes almost ok stacks of boxes. not ok because of scaling transformations
+	       +geom_bar(aes(group=gr),fill="grey", colour="black", width=min(1/10*10^p, 0.05),size=0.1)
+	       + geom_vline(xintercept=div, color="red", size=1.1)
+	       # + geom_vline(xintercept=dfun(ab,1), color="blue", linetype=3, size=1)
+	       # + geom_vline(xintercept=dfun(ab,-1), color="purple", linetype=4, size=1)
+	       # + geom_vline(xintercept=dfun(ab,0), color="green", linetype=5, size=1)
+	     #This makes a nice line that doesn't ahve issues with scaling
 	# + geom_segment(aes(x=rarity, xend=rarity, y=abundance, yend=0), size=1.6)
 		#could probably set breaks more flexibly
 		+ scale_x_continuous(trans=power_trans(pow=p))
 		#the expand=c(0,0) is what fixes x-axis in place at y=0
 		+ scale_y_continuous(expand=c(0,0))
+
 		#this is what makes the axis lines... it is just a line segment from min to max of the provided data. teh breaks are provided in scale_x_continuous or scale_y_continuous, wich in turn can get them from 
 		+ geom_rangeframe(data=data.frame(rarity=c(min(rf$rarity), max(rf$rarity)), abundance=c(0,max(abundance)+10)))
+	# + geom_point(aes(x=div, y=-2, color="red"))
 	
 	    + theme(legend.position="none")
 		
@@ -74,12 +79,16 @@ rarity_plot <- function(abundance, p){
 
 
 
-# ab <- c(20,30,50)
+ab <- c(20,30,50)
 # ab<-c(100, 20, 15, 10, 2, 1, 1,1)
-ab<-c(50,20,30,5,3,2)
+# ab<-c(50,20,30,5,3,2)
+
+library(grid)
 
 
-rarity_plot(ab, 1)
-rarity_plot(ab, 0)
-rarity_plot(ab, -1)
+p<-function(x){ggdraw(x %>% add_sub(label="^", size=20, colour="blue", x=0.351, y=1.4, vjust=0))}
 
+
+p(rarity_plot(ab,1))
+p(rarity_plot(ab, 0))
+p(rarity_plot(ab, -1))
---
+++ test_finite_simpson_estimators.R
@@ -0,0 +1,3 @@
+# to test:
+
+#Dushoff's finite-size simpson Hill estimator, vs. Simpson's estimator, vs. Perez et al.'s finite and inifinite simpson estimators, on simulated data with and without replacement. 
