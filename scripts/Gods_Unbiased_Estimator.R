574f785c720caab234c5c7518a87ce5e9571dacb
mikeroswell <mike.roswell@gmail.com>
2019-02-23 10:41:52 -0500

    actually made graphs. Looks like deal when N=30 and richness could use some investigation

--- scripts/Gods_Unbiased_Estimator.R
+++ scripts/Gods_Unbiased_Estimator.R
@@ -39,95 +39,90 @@ GUE<-function(freqs, true_p,l){
 begin<-1
 end<-99
 
- 
-# sim_sad(30, 100000, sad_type="gamma", sad_coef=list(shape=3,scale=3))
-# sim_sad(30, 100000, sad_type="powbend", sad_coef=list(s=1, omega=0.01))
-# sim_sad(30, 100000, sad_type="powbend", sad_coef=list(s=1, omega=0.04))
-# sim_sad(30, 100000, sad_type="geom", sad_coef=list(prob=.04))
-# sim_sad(s_pool=NULL,n_sim=100000, sad_type="ls", sad_coef=list(alpha=4, N=100000), fix_s_sim=30)
-# sim_sad(s_pool = 30, n_sim = 10000, sad_type = "poilog",
-#         sad_coef = list("mu" = 5, "sig" = 0.5))
-# sim_sad(s_pool = 30, n_sim = 10000, sad_type = "lnorm",
-#         sad_coef = list("meanlog" = 5, "sdlog" = 0.5))
-# sim_sad(s_pool = 30, n_sim = 10000, sad_type = "lnorm",
-#         sad_coef = list("meanlog" = 5, "sdlog" = 3))
-
-
 #make some different sads with different distributions, eveness, etc. for functions that can take S, N as arguments
-s_type<-rep(c("powbend", "geom", "lnorm"), 4)
+# s_type<-rep(c("powbend", "geom", "lnorm"), 4)
+# 
+# c_list<-list(
+#       list(s=1, omega=0.01), list(prob=0.01), list(meanlog=5, sdlog=.5)
+#      , list(s=1, omega=0.02), list(prob=0.02), list(meanlog=5, sdlog=3)
+#      , list(s=1, omega=0.04), list(prob=0.04), list(meanlog=5, sdlog=5)
+#      , list(s=1, omega=0.03), list(prob=0.03), list(meanlog=5, sdlog=1)
+#     )
+# 
+# 
+# nc<-parallel::detectCores()-1
+# plan(strategy=multiprocess, workers=nc)
+# sz<-round(exp(seq(9,19, 0.5)))
+# future_map(begin:end, function(x){
+#    
+#     map(c(30,75,150), function(S){
+#       
+#         out<-map_dfr(sz,function(comsize){
+#         #number of individuals in sample
+#             #SADs with ~S species
+#             map_dfr(1:length(s_type),function(saddef){
+#                     
+#                     a<-as.numeric(
+#                         # sim_sad(30, 100000, sad_type="powbend", sad_coef=list(s=1, omega=0.01))
+#                         sim_sad(
+#                             S
+#                             , comsize
+#                             # , sad_type="powbend"
+#                             , sad_type = s_type[[saddef]]
+#                             , sad_coef= c_list[[saddef]]
+#                             # , sad_coef =list(s=1, omega=0.01)
+#                             , fix_s_sim = T
+#                             )
+#                         )
+#                     true_p=a/sum(a)
+#                     
+#                         #Sample N from it to get observations
+#                     map_dfr(c(100, 200, 300, 500), function(N){
+#                         obs_namelist<-sample(1:S, size=N, replace=T, prob=true_p)
+#                         freqs<-unlist(lapply(1:S, function(x){length(which(obs_namelist==x))}))
+#                         # for each ell value
+#                         map_dfr(seq(-1,1,1), function(l){
+#                             true<-GUE(true_p, true_p, l)
+#                             gods<-GUE(freqs/sum(freqs), true_p, l)   
+#                             naive<-GUE((freqs/sum(freqs))[freqs/sum(freqs)!=0],(freqs/sum(freqs))[freqs/sum(freqs)!=0],l) # equals sum(freqs>0) for richness
+#                             chao<-Chao_Hill_abu(freqs, 1-l)
+#                             # simple_good1<-GT1(freqs)
+#                             # good_plugin<-GT2(freqs)
+#                             
+#                             
+#                             
+#                             return(data.frame(true, gods, naive, chao, l, N,S, comsize, saddef))
+#                     })
+#                 })
+#                     
+#             })
+#                 
+#         })
+#        
+#         write.csv(out, file=paste("data/GUEsims", S, "no", x, ".csv", sep=""))
+#            
+#     })
+# })
 
-c_list<-list(
-      list(s=1, omega=0.01), list(prob=0.01), list(meanlog=5, sdlog=.5)
-     , list(s=1, omega=0.02), list(prob=0.02), list(meanlog=5, sdlog=3)
-     , list(s=1, omega=0.04), list(prob=0.04), list(meanlog=5, sdlog=5)
-     , list(s=1, omega=0.03), list(prob=0.03), list(meanlog=5, sdlog=1)
-    )
 
-
-nc<-parallel::detectCores()-1
-plan(strategy=multiprocess, workers=nc)
-sz<-round(exp(seq(9,19, 0.5)))
-future_map(begin:end, function(x){
-   
-    map(c(30,75,150), function(S){
-      
-        out<-map_dfr(sz,function(comsize){
-        #number of individuals in sample
-            #SADs with ~S species
-            map_dfr(1:length(s_type),function(saddef){
-                    
-                    a<-as.numeric(
-                        # sim_sad(30, 100000, sad_type="powbend", sad_coef=list(s=1, omega=0.01))
-                        sim_sad(
-                            S
-                            , comsize
-                            # , sad_type="powbend"
-                            , sad_type = s_type[[saddef]]
-                            , sad_coef= c_list[[saddef]]
-                            # , sad_coef =list(s=1, omega=0.01)
-                            , fix_s_sim = T
-                            )
-                        )
-                    true_p=a/sum(a)
-                    
-                        #Sample N from it to get observations
-                    map_dfr(c(100, 200, 300, 500), function(N){
-                        obs_namelist<-sample(1:S, size=N, replace=T, prob=true_p)
-                        freqs<-unlist(lapply(1:S, function(x){length(which(obs_namelist==x))}))
-                        # for each ell value
-                        map_dfr(seq(-1,1,1), function(l){
-                            true<-GUE(true_p, true_p, l)
-                            gods<-GUE(freqs/sum(freqs), true_p, l)   
-                            naive<-GUE((freqs/sum(freqs))[freqs/sum(freqs)!=0],(freqs/sum(freqs))[freqs/sum(freqs)!=0],l) # equals sum(freqs>0) for richness
-                            chao<-Chao_Hill_abu(freqs, 1-l)
-                            # simple_good1<-GT1(freqs)
-                            # good_plugin<-GT2(freqs)
-                            
-                            
-                            
-                            return(data.frame(true, gods, naive, chao, l, N,S, comsize, saddef))
-                    })
-                })
-                    
-            })
-                
-        })
-       
-        write.csv(out, file=paste("data/GUEsims", S, "no", x, ".csv", sep=""))
-           
+guesims<-map_dfr(dir("data"), function(x){
+        if(grepl("GUEsims",x)){
+        read_csv(paste("data/", x, sep=""))}
     })
-})
+
+
 
 rootms<-function(x){sqrt(sum(((x)^2)/length(x)))}
 
-errs<-s %>% group_by(l,S,N,saddef) %>% mutate(ge=gods-true, ne=naive-true, ce=chao-true) %>% summarize_at(.vars=c("ge", "ne", "ce"), .funs=rootms) %>% gather(est, err,ge, ne, ce)
+errs<-guesims %>% group_by(l,S,N,comsize,saddef) %>% mutate(ge=gods-true, ne=naive-true, ce=chao-true) %>% summarize_at(.vars=c("ge", "ne", "ce"), .funs=rootms) %>% gather(est, err,ge, ne, ce)
 
-s %>% filter(S==30) %>% ggplot(aes(true, chao))+geom_point()
+# s %>% filter(S==30) %>% ggplot(aes(true, chao))+geom_point()
 
-(errs %>% ggplot(aes(N, err, color=est, shape=saddef))
-    +geom_jitter(width=12, alpha=0.4, size=3)
+(errs %>% ggplot(aes(N, err+1, color=est, shape=as.factor(comsize)))
+    +geom_jitter(width=40, alpha=0.2)
     +theme_classic()
     +facet_wrap(~S+l)
+    +scale_y_log10()
     
 )
 
