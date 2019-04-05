21d8e5896a0aeb254e6d67dab825e54cf7123567
mikeroswell <mike.roswell@gmail.com>
2019-04-05 10:28:13 -0400

    I think this is close enough to try on annotate and get data

--- scripts/diversity_checkplots.R
+++ scripts/diversity_checkplots.R
@@ -4,71 +4,100 @@ library(furrr)
 source("scripts/helper_funs/estimation_funs.R")
 library(mobsim)
 
-#start with JD comms
-mini<-100
-com1 <- c(1, 1, 1, 1)
-com2 <- c(1, rep(1/(mini-1), mini))
-#add a more naturalistic one
-com3<-as.numeric(sim_sad(s_pool=60, n_sim=100000))
-#going to take samples of size inds from each
+# #start with JD comms
+# mini<-100
+# com1 <- c(1, 1, 1, 1)
+# com2 <- c(1, rep(1/(mini-1), mini))
+# #add a more naturalistic one
+# com3<-as.numeric(sim_sad(s_pool=60, n_sim=100000))
+# #this is supersampling
+# o1<-sample(1:length(com1), prob=com1, size=inds, replace=T)
+# o2<-sample(1:length(com2), prob=com2, size=inds, replace=T)
 
-inds<-350
- #this is supersampling
-o1<-sample(1:length(com1), prob=com1, size=inds, replace=T)
-o2<-sample(1:length(com2), prob=com2, size=inds, replace=T)
-#this is subsampling
-o3<-subsam(com3, size=inds)
+#three communities with richness ~60, and different skew
+com1<-as.numeric(sim_sad(s_pool=60, n_sim=100000, sad_coef=list(cv_abund=2)))
+com2<-as.numeric(sim_sad(s_pool=60, n_sim=100000, sad_coef=list(cv_abund=5)))
+com3<-as.numeric(sim_sad(s_pool=60, n_sim=100000, sad_coef=list(cv_abund=3)))
+
+plot(1:length(com1), com1)
+plot(1:length(com2), com2)
+plot(1:length(com3), com3)
+
 
 asab<-function(namevec){as.numeric(table(namevec))}
 
-dfun(asab(o1), l=-1)
-dfun(asab(o2), l=-1)
 dfun(com1, l=-1)
 dfun(com2, l=-1)
 dfun(com3, l=-1)
-dfun(o3, l=-1)
 
 #gets slightly closer than obs
-Chao_Hill_abu(asab(o1), q=2)
-#also slightly closer than obs but still far off and downward biased?
-Chao_Hill_abu(asab(o2), q=2)
-
-Chao_Hill_abu(o3, q=2)
-# checkchao(asab(o1), B=1000, l=-1, truediv=4)
 
 nc<-parallel::detectCores()-1
 plan(strategy=multiprocess, workers=nc)
 
-abs<-o3
-B<-1000
-l<--1
-truediv<-dfun(com3, l=-1)
+# make this cleaner
+checkplot<-function(abs, B=2000, l, inds, reps){
+    td<-dfun(abs, l)
+    future_map_dfr(1:reps,function(x){
+    obs<-subsam(abs, size=inds)
+    chaotile<-checkchao(obs, B, l, td)
+   return(chaotile=data.frame(chaotile, l=rep(l, reps), inds=rep(inds, reps), reps=rep(reps, reps)))})
+   }
 
-do1000<-function(abs, B, l, truediv){future_map_dfr(1:1000,function(x){
-    chaotile<-checkchao(abs, B, l, truediv)
-    return(chaotile=data.frame(chaotile))})
-    }
-
-
-
-
-try1000times<-map_dfr(1:1000, function(x){
-    o3<-subsam(com3, size=inds)
-    return(do1000(o3, 1000, -1, dfun(com3, -1)))
+reps<-10
+start<-Sys.time()
+tall<-map_dfr(c(150,300,750), function(inds){
+    map_dfr(c("com1", "com2", "com3"), function(abs){
+        map_dfr(c(-1,0,0.5,1), function(l){
+            out<-checkplot(abs=get(abs), l=l, inds=inds, reps=reps)
+            return(cbind(out, comm=rep(abs, length(out[,1]))))
+        })
+    })
 })
+print(Sys.time()-start)
 
-firstout<-do1000(o3, 1000, -1, dfun(com3,-1))
-pdf(file="figures/first_naturalistic_Simpson_checkplot.pdf")
-hist(firstout$chaotile, xlab="true value as percentile of Chao boot", xlim=c(0,100), main=paste("naturalistic lognormal community with Simpson-Hill=", round(truediv,2)))
-dev.off()
 
-firstout<-do1000(asab(o1), 1000, -1, dfun(com1,-1))
-pdf(file="figures/first_even_Simpson_checkplot.pdf")
-hist(firstout$chaotile, xlab="true value as percentile of Chao boot", xlim=c(0,100), main="even community with Simpson-Hill=4")
-dev.off()
+# t100<-do1000(com1, B=2000, l=1, inds=150, reps=100)
 
-firstout<-do1000(asab(o2), 1000, -1, dfun(com2,-1))
-pdf(file="figures/first_Simpson_checkplots.pdf")
-hist(firstout$chaotile, xlab="true value as percentile of Chao boot", xlim=c(0,100), main="skewed community with Simpson-Hill=4")
-dev.off()
+# 
+# try1000timesSHan<-map_dfr(1:1000, function(x){
+#     o3<-subsam(com3, size=inds)
+#     return(do1000(o3, 1000, 0, dfun(com3, 0)))
+# })
 
+# try200times.5<-map_dfr(1:200, function(x){
+#     o3<-subsam(com3, size=inds)
+#     return(do1000(o3, 400, .5, dfun(com3, .5)))
+# })
+# 
+# sum(try200times.5$chaotile<2.5)/1000000+sum(try200times.5$chaotile>97.5)/1000000
+# 
+# 
+# 
+# sum(try1000timesSHan$chaotile<2.5)/1000000+sum(try1000timesSHan$chaotile>97.5)/1000000
+# 
+# pdf(file="figures/repeated_checkplot_with_same_comm_point5.pdf")
+# hist(try200times.5$chaotile, xlab="true value as percentile of Chao boot", xlim=c(0,100), main="200 random draws from naturalistic community \n l=0.5")
+# dev.off()
+# 
+# hist(try200times.5$chaotile[1601:2000], xlab="true value as percentile of Chao boot", xlim=c(0,100), main="200 random draws from naturalistic community \n l=0.5")
+# 
+# pdf(file="figures/repeated_checkplot_with_same_comm_SH.pdf")
+# hist(try100timesSHan$chaotile, xlab="true value as percentile of Chao boot", xlim=c(0,100), main="1000 random draws from naturalistic community \n rich")
+# dev.off()
+# 
+# firstout<-do1000(o3, 1000, 0, dfun(com3,0))
+# pdf(file="figures/first_naturalistic_Shannon_checkplot.pdf")
+# hist(firstout$chaotile, xlab="true value as percentile of Chao boot", xlim=c(0,100), main=paste("naturalistic lognormal community with Shannon-Hill=", round(truediv,2)))
+# dev.off()
+# 
+# firstout<-do1000(asab(o1), 1000, -1, dfun(com1,-1))
+# pdf(file="figures/first_even_Simpson_checkplot.pdf")
+# hist(firstout$chaotile, xlab="true value as percentile of Chao boot", xlim=c(0,100), main="even community with Simpson-Hill=4")
+# dev.off()
+# 
+# firstout<-do1000(asab(o2), 1000, -1, dfun(com2,-1))
+# pdf(file="figures/first_Simpson_checkplots.pdf")
+# hist(firstout$chaotile, xlab="true value as percentile of Chao boot", xlim=c(0,100), main="skewed community with Simpson-Hill=4")
+# dev.off()
+# 
