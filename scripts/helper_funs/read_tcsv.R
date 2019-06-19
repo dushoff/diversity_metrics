e1f29f9c8521a37da39a5e6a9e75288c761fc307
mikeroswell <mike.roswell@gmail.com>
2019-06-19 17:13:11 -0400

    found a nice read.csv function online. now running with Haegeman data.

---
+++ scripts/helper_funs/read_tcsv.R
@@ -0,0 +1,22 @@
+#################
+# nice function to read transposed .csvs
+
+read.tcsv = function(file, header=TRUE, sep=",", ...) {
+  
+  n = max(count.fields(file, sep=sep), na.rm=TRUE)
+  x = readLines(file)
+  
+  .splitvar = function(x, sep, n) {
+    var = unlist(strsplit(x, split=sep))
+    length(var) = n
+    return(var)
+  }
+  
+  x = do.call(cbind, lapply(x, .splitvar, sep=sep, n=n))
+  x = apply(x, 1, paste, collapse=sep) 
+  out = read.csv(text=x, sep=sep, header=header, ...)
+  return(out)
+  
+}
+
+## https://stackoverflow.com/a/17289991/8400969
