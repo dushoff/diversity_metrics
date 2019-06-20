ce0cc5f1d72de32b60bb070e99158c47676a2042
Jonathan Dushoff <dushoff@mcmaster.ca>
2019-06-20 17:58:20 -0400

    Trying to diagnose Chao narrowness

--- scripts/richness_stats.R
+++ scripts/richness_stats.R
@@ -1,5 +1,7 @@
 ### Ripping off Mike R. and trying to test statistical coverage for Chao richness
 
+lmin <- 1
+lmax <- 1
 s_pool=60 # Pool 
 n_ind=1e3 #number of individuals to simulate
 sad_coef=list(cv_abund=5)
@@ -8,8 +10,9 @@ minind<-1e2
 maxind<-3.2e2
 indSteps <- 3 #this determines how many sample sizes between minind and maxind to take
 
-bootSamps<-4e2 # Ideally should be higher
-reps<-5e1
+bootSamps<-2e2 
+checkSamps<-2e3
+muReps<-2e3
 
 #############
 #load functions and packages
@@ -19,8 +22,6 @@ library(furrr)#parallelization
 library(scales)#trans_breaks
 library(mobsim)#simulate communities
 library(cowplot) #sometimes nice stuff for multipanel ggplotting
-
-select<-dplyr::select
 
 #make a community for User's Guide
 #sim_sad 
@@ -38,7 +39,8 @@ plan(strategy=multiprocess, workers=nc)
 obscp<-function(l, sampleSize, dat, bootSamps, truemun){
 	q <- 1-l
 	sam<-subsam(dat, sampleSize)
-	data.bt = rmultinom(bootSamps,sampleSize,Bt_prob_abu(sam))
+	aProb <- Bt_prob_abu(sam)
+	data.bt = rmultinom(bootSamps,sampleSize,aProb)
 	obs<-dfun(sam,l)
 	pro = apply(data.bt,2,function(boot)Chao_Hill_abu(boot,q)) 
 	pro<-pro-mean(pro)+obs
@@ -49,37 +51,48 @@ obscp<-function(l, sampleSize, dat, bootSamps, truemun){
 	)) 
 }
 
-####################
-#run this whole thing to get sample diversity checkplot-type info for sample diversity for a single community
-indSeq <- round(exp(seq(log(minind), log(maxind), length.out=indSteps)))
-trycheckingobs_R<-map_dfr(indSeq, function(sampleSize){
-	truemun<-truemu(usersguide, size=sampleSize, reps=reps, l=1)
-	future_map_dfr(1:reps, function(reps){
-	 	obscp(l=1, sampleSize, bootSamps, usersguide, truemun)
+######################################################################
+
+## For stepping; doesn't hurt much
+## Can be deleted when we understand things better
+l <- 1
+dat <- usersguide
+sampleSize <- 200
+
+truemun<-truemu(usersguide, size=sampleSize, reps=muReps, l=l)
+
+q <- 1-l
+sam<-subsam(dat, sampleSize)
+aProb <- Bt_prob_abu(sam)
+aProb[is.na(aProb)] <- 0
+data.bt = rmultinom(bootSamps,sampleSize,aProb)
+obs<-dfun(sam,l)
+pro = apply(data.bt,2,function(boot)Chao_Hill_abu(boot,q)) 
+pro<-pro-mean(pro)+obs
+chaotile<-sum(pro<=truemun)/(bootSamps/100)
+
+
+######################################################################
+## Main loop
+
+for(l in lmin:lmax){
+	indSeq <- round(exp(seq(log(minind), log(maxind), length.out=indSteps)))
+	checkFrame <-map_dfr(indSeq, function(sampleSize){
+		truemun<-truemu(usersguide, size=sampleSize, reps=muReps, l=l)
+		future_map_dfr(1:checkSamps, function(r){
+			obscp(l=l, sampleSize, usersguide, bootSamps, truemun)
+		})
 	})
-})
 
-write.csv(trycheckingobs_R, file="csvname", row.names=F)
-
-quit()
-
-##################################
-# read in data and make checkplot for sample diveristy
-trycheckingobs<-read.csv("data/fromR/trycheckingobs_R.csv")
-
-#####################
-# #checkplot figure, not used in users guide
-# map(c(-1,0,1), function(ell){
-#		trycheckingobs %>% filter(l==ell) %>% 
-#			ggplot(aes(chaotile))+
-#			geom_histogram()+
-#			theme_classic()+
-#			facet_wrap(~size+l)
-# })
-
-trycheckingobs_R %>% 
-	ggplot(aes(chaotile))+
-	geom_histogram()+
-	theme_classic()+
-	labs(x=paste("percentile of mean sample richness in", actual_boot, "bootstrap samples"), y="frequency")+
-	facet_wrap(~size)
+	print(ggplot(checkFrame, aes(chaotile))
+		+ geom_histogram()
+		+ theme_classic()
+		+ labs(
+			x=paste("percentile of sample diversity in"
+				, bootSamps, "bootstrap samples"
+			)
+			, y="frequency"
+		)
+		+ facet_wrap(~sampleSize)
+	)
+}
