116dbbc541d66593b1847861e4b4d34f330f19e9
mikeroswell <mike.roswell@gmail.com>
2019-08-01 12:07:25 -0400

    This is a good stopping place.

--- scripts/rshiny_app1/server.R
+++ scripts/rshiny_app1/server.R
@@ -1,21 +1,27 @@
 # Define server logic required to draw a rarity plot ----
 server <- function(input, output, session) {
-    
+
     # rarity plot for sample community ----
     # with user-chosen scaling exponent, input abundances, and choice of lines or boxes
     # This expression that generates a rarity plot is wrapped in a call
     # to renderPlot to indicate that:
-    #
+    #''
     # 1. It is "reactive" and therefore should be automatically
     #    re-executed when inputs change
     # 2. Its output type is a plot
+
+    
     output$rarityPlot <- renderPlot({
-        
-        source("balance.R")
-        abus<-as.numeric(unlist(strsplit(input$abundances,",")))
-        rarity_plot(abus, input$ell, lines=input$line)
+       
+        rarity_plot(as.numeric(unlist(strsplit(input$abundances,","))), input$ell, lines=input$line)
         
     })
+  
+    
+    output$mean_rarity<-renderText({
+        paste("mean rarity = ",     dfun(as.numeric(unlist(strsplit(input$abundances,","))), input$ell), "; ell = ", input$ell, sep="")
+    })
+    
     output$legend4app <- renderPlot({
         
         source("balance.R")
