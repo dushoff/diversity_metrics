e68b6c63d81298ddf7ca6e15e42b6ff85272d996
mikeroswell <mike.roswell@gmail.com>
2019-08-02 06:51:40 -0400

    minor revisions

--- scripts/rshiny_app1/server.R
+++ scripts/rshiny_app1/server.R
@@ -1,6 +1,10 @@
 # Define server logic required to draw a rarity plot ----
-server <- function(input, output, session) {
+checknumeric<-function(x){tryCatch(is.numeric(as.numeric(unlist(strsplit(x,",")))^2), error=function(e)e)}
 
+server <- function(input, output, session) {
+    
+   
+    
     # rarity plot for sample community ----
     # with user-chosen scaling exponent, input abundances, and choice of lines or boxes
     # This expression that generates a rarity plot is wrapped in a call
@@ -10,8 +14,17 @@ server <- function(input, output, session) {
     #    re-executed when inputs change
     # 2. Its output type is a plot
 
+# abus <- renderTable({
+#         # validate(
+#         #     need(is.vector(c(input$abundances)))
+#         # )
+#     input$abundances
+#     # as.numeric(unlist(strsplit(input$abundances,",")))
+#     })
     
     output$rarityPlot <- renderPlot({
+        # validate(checknumeric(input$ell))
+        # validate(need((as.numeric(unlist(strsplit(input$abundances,",")))), "uhoh"))
        
         rarity_plot(as.numeric(unlist(strsplit(input$abundances,","))), input$ell, lines=input$line)
         
@@ -30,10 +43,11 @@ server <- function(input, output, session) {
     })
     output$myEqs <- renderUI({
         withMathJax(
-            helpText( 'Hill diversity can be expressed as the mean species rarity: $$D=[ \\sum_{i=1}^S p_i(1/p_i)^l]^\\frac{1}{l}$$')
+            helpText( 'Hill diversity can be expressed as the mean species rarity: $$D=[ \\sum_{i=1}^S p_i(r_i)^l]^\\frac{1}{l}$$')
             , helpText("where", em("S"),' = total number of species,')
-            , helpText('$$p_i= \\frac{abundance_i}{\\sum_{i=1}^Sabundance_i}$$')
-            , helpText('rarity = 1/', em("p"),",")
+            # , helpText('$$p_i= \\frac{abundance_i}{\\sum_{i=1}^Sabundance_i}$$')
+            , helpText(em("p_i"), "= relative abundance of species", em("i"),",")
+            , helpText(em("r_i"), "= rarity  of species", em("i"), "= 1/", em("p_i"),",")
             , helpText("and", em("l"),' is the control parameter that scales the rarity axis.')
             , helpText('In this program,', em("D"),' is shown as the fulcrum of the lever. The arithmetic, geometric, and harmonic means are included as reference points for each scale.')
             )
