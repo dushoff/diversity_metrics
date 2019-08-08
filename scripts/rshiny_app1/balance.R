9764ebadb2d07f618fe83b47b35c8f69e19688d4
mikeroswell <mike.roswell@gmail.com>
2019-08-08 14:10:40 -0400

    never liked this plot anyways

--- scripts/rshiny_app1/balance.R
+++ scripts/rshiny_app1/balance.R
@@ -270,19 +270,3 @@ ab<-c(20,8,5,4,2,1) #candidate for user's guide
 # grid.arrange(p, omit_y(p), omit_y(p), p, omit_y(p), omit_y(p), p, omit_y(p), omit_y(p))
 
 # rarity_series(ab=ab, 1:-1)
-
-# legend plot
-meanpoints<-data.frame(hght=c(-1,0,1), ell=c("l=-1", "l=0", "l=1"), meantype=c(" harmonic mean \n     l=-1", " geometric mean \n      l=0", " arithmetic mean \n     l=1") )
-
-legend_for_app<-meanpoints %>% 
-    ggplot(aes( x=5*hght,y=c(0.5,0.5,0.5), color=ell))+
-    geom_point(size=3)+
-    geom_text(aes(label=meantype),hjust=0,vjust=1, size=6 )+ 
-    # geom_point(data=data.frame(x=c(0,0), y=c(-0.3,0.3)), aes(x,y), color="white")+
-    scale_color_brewer(type="qual", palette="Dark2",guide="none")+
-    theme_void()+
-    theme(plot.margin=unit(c(0,1,0,1),"cm"))+
-    scale_y_continuous(limits=c(0, 1))+
-    scale_x_continuous(limits=c(-7,10))
-    
-    
