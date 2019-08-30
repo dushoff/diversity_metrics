0346b3697aaf05131b6f2bc692030eefeaf9c10c
mikeroswell <mike.roswell@gmail.com>
2019-08-30 10:05:17 -0400

    Fixed various things, abundances now integer values!

--- scripts/uniroot_gamma_and_lnorm.R
+++ scripts/uniroot_gamma_and_lnorm.R
@@ -1,19 +1,20 @@
-# play with uniroot and figure out how to generate SADS based on Richness and Simpson
+# Generates a semi-parametric SAD based on richness, and simpson, and total abundance. Requires a bit of thought b/c function requires a "prior" for the "shape" parameter of either "gamma" or "lnorm." For gamma, seems like if it tries the a shape parameter too close to 0, it returns garbage in the form of ur_gamma(0)==-Inf.
+
 source("scripts/helper_funs/estimation_funs.R")
 
 #define variables but also give them values for messing with
-totAb<-1e7 #total true abundance in community
-rich<-50 # true richness
-simpson<-40 #true simpson
+# totAb<-1e7 #total true abundance in community
+# rich<-50 # true richness
+# simpson<-40 #true simpson
 
 
 #define my gamma distribution
 discrete_gamma<-function(rich, totAb, x){
-    qgamma(seq((1/rich)/2, 1-(1/rich)/2, (1/rich)), shape=x, scale=(totAb/rich)/x)
+    round(qgamma(seq((1/rich)/2, 1-(1/rich)/2, (1/rich)), shape=x, scale=(totAb/rich)/x))
     }
 
 #function for uniroot to optimize, according to simpsons
-ur_gamma<-function(x,...){simpson-dfun(discrete_gamma(rich, totAb,x), -1)}
+ur_gamma<-function(x,rich=rich, simpson=simpson, totAb=totAb,...){simpson-dfun(discrete_gamma(rich, totAb,x), -1)}
 
 # gamShape<-uniroot(ur_gamma, lower=1e-2, upper=1e2)
 # gamShape
@@ -22,23 +23,33 @@ ur_gamma<-function(x,...){simpson-dfun(discrete_gamma(rich, totAb,x), -1)}
 
 #define my lognormal distribution
 discrete_lnorm<-function(rich, totAb, x){
-    qlnorm(seq((1/rich)/2, 1-(1/rich)/2, (1/rich)), meanlog=log((totAb/rich)^2/sqrt(x^2+(totAb/rich)^2)), sdlog=(log(1+x^2/(totAb/rich)^2))^(1/2))
+    round(qlnorm(seq((1/rich)/2, 1-(1/rich)/2, (1/rich)), meanlog=log((totAb/rich)^2/sqrt(x^2+(totAb/rich)^2)), sdlog=(log(1+x^2/(totAb/rich)^2))^(1/2)))
     }
 
-ur_lognorm<-function(x,...){ ddiff=simpson-dfun(discrete_lnorm(rich, totAb, x), -1)
+ur_lognorm<-function(x, rich=rich, simpson=simpson, totAb=totAb,...){ ddiff=simpson-dfun(discrete_lnorm(rich, totAb, x), -1)
     return(ddiff)} #here x is arithmetic sd of lognormal.
 
 fit_SAD<-function(totAb=1e7, rich=50, simpson=40, int_lwr=0,int_uppr=1e9, dstr="lnorm"){
+    #check feasibility
+    if(simpson>rich){return("ERROR: Hill-Simpson diversity cannot be greater than richness")}
+    
+    #check dstr makes sense
     ifelse( !(dstr %in% c("lnorm", "gamma")), return("ERROR: dstr must be either `lnorm` or `gamma`"),
+            
+        #generate SAD when dstr=lnorm 
         {if(dstr=="lnorm"){
-                fit_par=tryCatch(uniroot(ur_lognorm, lower=int_lwr, upper=int_uppr), error=function(e) message("ERROR: test int_lwr and int_uppr in ur_ function, output must have opposite signs"))
+            #uses an optimizer called uniroot to find x when ur_lognorm(x)==0
+                fit_par=tryCatch(uniroot(function(x){ur_lognorm(simpson=simpson, rich=rich, totAb=totAb,x)}, lower=int_lwr, upper=int_uppr), error=function(e) message("ERROR: test int_lwr and int_uppr in ur_ function, output must have opposite signs"))
                 abus=tryCatch(discrete_lnorm(rich, totAb, fit_par$root), error=function(e) {message("did not fit param")
                     return(rep(100, length(rich)))}
                 )
                 
         }
+            
+            #generate SAD when dstr="gamma"
         if(dstr=="gamma"){
-            fit_par=tryCatch(uniroot(ur_gamma, lower=int_lwr, upper=int_uppr), error=function(e) message("ERROR: test int_lwr and int_uppr in ur_ function, output must have opposite signs"))
+            #uses an optimizer called uniroot to find x when ur_gamma(x)==0
+            fit_par=tryCatch(uniroot(function(x){ur_gamma(simpson=simpson, rich=rich, totAb=totAb, x)}, lower=int_lwr, upper=int_uppr), error=function(e) message("ERROR: test int_lwr and int_uppr in ur_ function, output must have opposite signs"))
             abus=tryCatch(discrete_gamma(rich, totAb, fit_par$root), error=function(e) {
                 message("did not fit param")
                 return(rep(100, length(rich)))
@@ -48,7 +59,7 @@ fit_SAD<-function(totAb=1e7, rich=50, simpson=40, int_lwr=0,int_uppr=1e9, dstr="
         shannon=dfun(abus, 0)
         return(list("distribution_info"=c("distribution"=dstr, "fitted parameter"=fit_par$root)
                     , "abundances"=abus
-                    , "diversities"=c("richness"=rich, "Shannon-Hill"=shannon, "Simpson-Hill"=simpson)
+                    , "diversities"=c("richness"=rich, "Hill-Shannon"=shannon, "Hill-Simpson"=simpson)
                    ))
         }
     )
@@ -58,4 +69,9 @@ fit_SAD<-function(totAb=1e7, rich=50, simpson=40, int_lwr=0,int_uppr=1e9, dstr="
 fit_SAD(dstr="lnorm")
 fit_SAD(dstr="nonsense")  
 fit_SAD(dstr="gamma")
-fit_SAD(dstr="gamma", int_lwr=1e-2)        
+fit_SAD(dstr="gamma", int_lwr=1e-2)  
+fit_SAD(dstr="lnorm", rich=50, simpson=90)
+fit_SAD(dstr="lnorm", rich=50, simpson=2)
+fit_SAD(dstr="lnorm", rich=50, simpson=2, int_uppr=1e15)
+
+
