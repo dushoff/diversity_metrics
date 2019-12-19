8f79809fb724487a429014848117fe00008fad57
mikeroswell <mike.roswell@gmail.com>
2019-12-19 18:23:50 -0500

    going to try obs which might have its own issues.

---
+++ scripts/obs_SAD1.R
@@ -0,0 +1,13 @@
+source("scripts/checkplot_initials.R")
+source("scripts/obscp_inf.R")
+reps<-5e3
+outerreps<-400
+Bnum<-2e3
+nc<-125#per Rob's recommendation
+plan(strategy=multiprocess, workers=nc)
+map(1:10, function(tryme){
+        start<-Sys.time()
+nd<-trycheckingobs(flatten(flatten(SADs_list))[[1]])
+write.csv(nd, file=paste("data/new_trycheckingobs_SAD_1", "iter_", tryme, ".csv", sep=""), row.names=F)
+print(Sys.time()-start)
+})
