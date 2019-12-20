4a97272cf3b9fd66bdbe79667a9904b8ad172be0
mikeroswell <mike.roswell@gmail.com>
2019-12-20 13:17:34 -0500

    upping memory request

--- ObsJob.sh
+++ ObsJob.sh
@@ -4,7 +4,7 @@
 #SBATCH --job-name=several_div # Name of the job 
 #SBATCH --ntasks=1 # Number of tasks 
 #SBATCH --cpus-per-task=6 # Number of CPUs per task 
-#SBATCH --mem=650GB # Requested memory 
+#SBATCH --mem=1500GB # Requested memory 
 #SBATCH --array=0-23%8 # Array job will submit 24 jobs, 8 at a time
 #SBATCH --time=72:00:00 # Total run time limit (HH:MM:SS)
 #SBATCH --output=slurm.%N.%j.out # STDOUT file 
