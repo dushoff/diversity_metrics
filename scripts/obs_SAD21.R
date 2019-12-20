410c939082b72cd40ea37f2f9cb411d55c8fb7da
mikeroswell <mike.roswell@gmail.com>
2019-12-19 23:08:27 -0500

    new code that might have less mem issues

--- scripts/obs_SAD21.R
+++ scripts/obs_SAD21.R
@@ -5,9 +5,11 @@ outerreps<-400
 Bnum<-2e3
 nc<-125#per Rob's recommendation
 plan(strategy=multiprocess, workers=nc)
+ map_dfr(round(10^seq(2, 5.5, 0.25)), function(size){
 map(1:10, function(tryme){
         start<-Sys.time()
-nd<-trycheckingobs(flatten(flatten(SADs_list))[[21]])
-write.csv(nd, file=paste("data/new_trycheckingobs_SAD_21", "iter_", tryme, ".csv", sep=""), row.names=F)
+nd<-trycheckingobs(flatten(flatten(SADs_list))[[21]], size)
+write.csv(nd, file=paste("data/new_trycheckingobs_SAD_21", "iter_", tryme, "size", size, ".csv", sep=""), row.names=F)
 print(Sys.time()-start)
+})
 })
