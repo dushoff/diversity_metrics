a21496849764eb33500f49e58754c9cc23a843b3
mikeroswell <mike.roswell@gmail.com>
2019-12-23 12:35:17 -0500

    new shell script to try for asymptotic

---
+++ rerun_asymptotic.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+#SBATCH --partition=main 
+# Name of the partition 
+#SBATCH --job-name=asymptot # Name of the job 
+#SBATCH --ntasks=1 # Number of tasks 
+#SBATCH --cpus-per-task=6 # Number of CPUs per task 
+#SBATCH --mem=192GB # Requested memory 
+#SBATCH --array=0-23 # Array job will submit 24 jobs
+#SBATCH --time=72:00:00 # Total run time limit (HH:MM:SS)
+#SBATCH --output=slurm.%N.%j.out # STDOUT file 
+#SBATCH --error=slurm.%N.%j.err  # STDERR file 
+
+
+module load intel/17.0.4
+
+module load R-Project/3.4.1
+at=$SLURM_ARRAY_TASK_ID+1
+srun Rscript scripts/asy_SAD$at.R 
