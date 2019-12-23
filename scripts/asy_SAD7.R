62c8fe067b206aaf5a2675347303a7003a103d07
mikeroswell <mike.roswell@gmail.com>
2019-12-23 12:38:00 -0500

    not sure where I'm at but going to try this all on amarel again.

--- scripts/asy_SAD7.R
+++ scripts/asy_SAD7.R
@@ -2,9 +2,9 @@ source("scripts/checkplot_initials.R")
 source("scripts/checkplot_inf.R")
 reps<-125
 outerreps<-400
-nc<-125#per Rob's recommendation
+nc<-125
 plan(strategy=multiprocess, workers=nc)
-l<-0
+map(c(-1,0,1), function(l){
 map(1:outerreps, function(x){
     map(rev(round(10^seq(2, 5, 0.25))), function(size){
         start<-Sys.time()
@@ -12,5 +12,6 @@ out<-checkplot_inf(flatten(flatten(SADs_list))[[7]], l=l, inds=size, reps=reps)
 write.csv(out, paste("data/SAD7","l", l, "inds", size, "outer",  x, ".csv", sep="_"), row.names=F)
 print(Sys.time()-start)
 })
+  })
   })
 })
