caf83f5f178633e3cb98b5746f477b66d294ef01
mikeroswell <mike.roswell@gmail.com>
2019-12-28 13:05:04 -0500

    avoid errors from extra })

--- scripts/asy_45.R
+++ scripts/asy_45.R
@@ -4,7 +4,7 @@ reps<-50
 outerreps<-1000
 nc<-12
 plan(strategy=multisession, workers=nc)
-map(1:outerreps, function(x){
+map(rev(1:outerreps), function(x){
     map(rev(round(10^seq(2, 5, 0.25))), function(size){
         start<-Sys.time()
 out<-checkplot_inf(flatten(flatten(SADs_list))[[16]], l=-1, inds=size, reps=reps)
@@ -12,5 +12,4 @@ write.csv(out, paste("data/SAD16","l",-1,"inds", size, "outernew",  x, ".csv", s
 rm(out)
 print(Sys.time()-start)
 })
-  })
 })
