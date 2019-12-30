9b0bad9ec5a02689b1dc397547bd4fd4ea37ab87
mikeroswell <mike.roswell@gmail.com>
2019-12-30 11:37:01 -0500

    needs some cleanup but I'm making some potentially useful results with tons of data (but not enough tons, potentially)

--- scripts/early_diversity_checkplots.R
+++ scripts/early_diversity_checkplots.R
@@ -1,9 +1,18 @@
 ###############
 # file to read in checkplot data and make some CHECKPLOTS!
 library(tidyverse)
+#to make checkplots, path only makes sense on my computer
 source("/Rstudio_Git/checkPlots/checkFuns.R")
+#to get info about each SAD
+source("scripts/checkplot_initials.R")
 library(furrr)
 plan(strategy=multiprocess, workers=7)
+
+
+read_bigs<-function(etype, x){read.csv(paste0("data/",etype, "_SAD", x, ".csv"))}
+byl<-function(x){x %>% group_by(l) %>% summarize(n())}
+pst_names<-function(x){paste(map(1:length(x), function(y){paste0(names(x)[y], ": ",x[y])}),collapse = ", ")}
+
 
 
 ###########
@@ -19,21 +28,35 @@ Obs_SAD6<-read.csv("obs_SAD6.csv")
 Obs_SAD6 %>% group_by(l) %>% summarize(n())
 
 
+byl(read_bigs("obs", 2))
 
-read_bigs<-function(etype, x){read.csv(paste0(etype, "_SAD", x, ".csv"))}
-byl<-function(x){x %>% group_by(l) %>% summarize(n())}
+###########
+# going slow, figuring out how to make checkplot results look ok for all SADS, 
+#possible results figures drafts
+#will turn into a function if it works and make a pile of these.
 
-byl(read_bigs("asy", 2))
+##tester params
+SAD<-1
+ell<--1
+pdf("figures/SAD_2_obs_cp.pdf", width=11, height=8.5)
+future_map(1:24, function(SAD){
+  map(c(-1,0,1), function(ell){
+    SADinfo<-flatten(flatten(SADs_list))[[SAD]]
+    read_bigs("obs", SAD) %>% 
+      mutate(hilld=c("richness", "Hill-Shannon", "Hill-Simpson")[2-l]) %>%
+      mutate(hilld=factor(hilld, levels=c("richness", "Hill-Shannon", "Hill-Simpson"))) %>%
+      filter(l==ell) %>%
+      checkplot(facets=15)+
+      theme_classic()+
+      facet_wrap(~size, scales="free", nrow=3)+
+      theme(panel.spacing.x = unit(1, "lines"))+
+      scale_x_continuous(expand=c(0,0))+
+      ggtitle(paste0(pst_names(SADinfo$distribution_info), pst_names(SADinfo$community_info),collapse = ", "))
+  })
+})
+dev.off()
+  
 
-Obs_SAD23 %>% 
-  mutate(hilld=c("richness", "Hill-Shannon", "Hill-Simpson")[2-l]) %>%
-    mutate(hilld=factor(hilld, levels=c("richness", "Hill-Shannon", "Hill-Simpson"))) %>%
-  #   # filter(hilld!="Hill-Shannon") %>%
-  checkplot(facets=8)+
-  theme_classic()+
-  facet_grid(size~hilld, scales="free")+
-  theme(panel.spacing.x = unit(2, "lines"))+
-  scale_x_continuous(expand=c(0,0))
 
 
 pdf("figures/first_new_cps.pdf", height=4.5, width=8)
