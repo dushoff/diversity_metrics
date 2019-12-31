22164907938a83388911aa5178207be25057e85a
mikeroswell <mike.roswell@gmail.com>
2019-12-31 12:21:59 -0500

    This is getting much coooler. Modified the cleanup loop so that hopefully (and need to verify) it adds new data to existing data and saves as a csv with the same file name. The changes are not backed up in git b/c of file size though, which is a small concern. This is a message for the commit following the cleanup on my computer

--- scripts/trim_CSVs_initial.R
+++ scripts/trim_CSVs_initial.R
@@ -14,8 +14,9 @@ byl(one_obs)
 #next step is to repeat this but sending the newer .csvs to the data directory, then will bind rows from the ones in the home directory.
 
 map(1:24, function(SAD){
-  write.csv(future_map_dfr(1:1000, function(x){
-    
+  write.csv(bind_rows(
+    read.csv(paste0("data/asy_SAD", SAD,  ".csv"))
+             , future_map_dfr(1:1000, function(x){
         map_dfr(rev(round(10^seq(2, 4, 0.25))), function(size){
           map_dfr(c(-1,0,1), function(l){
                 
@@ -36,9 +37,31 @@ map(1:24, function(SAD){
                 return(data.frame(out))
           })
       })
-  })
+  }))
   , paste0("data/asy_SAD", SAD, ".csv"), row.names=F)
 })     
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+)
+  
+  
+  
+  
+  
+  
 
 #thsi combine: try bind rows of the old and the new. If it works sesms easy to recycles in the loop
 future_map(1:24, function(SAD){
