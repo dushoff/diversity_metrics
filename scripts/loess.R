2d0eecf132fdc163f3afad41d155598439bca873
mikeroswell <mike.roswell@gmail.com>
2020-03-24 09:55:55 -0400

    This workflow is totally opaque need to create something to stitch it all together here.

--- scripts/loess.R
+++ scripts/loess.R
@@ -8,21 +8,32 @@ bs<-fread("data/comm_samp.csv")
 bs_short<-bs[,-c(1:200)] %>%
     gather(dtype, div, rich, shan, simp)
 
-rm(bs)
+bs_sub <- bs_short %>% sample_n(5e3)
 
-combo<-bs_short %>% group_by(comm, dtype) %>% summarize(resi=n())
+
+pdf("figures/loess_smooth_5e4_points.pdf")
+bs_short %>% sample_n(5e3) %>% 
+    ggplot(aes(log(tc/(1-tc)), div, color=SS))+
+    geom_point(alpha=0.1)+
+    facet_grid(dtype~comm, scales="free_y")+
+    theme_classic()+
+    geom_smooth(method="loess", formula=y~x, color="red")#, se=F)
+dev.off()
+
+combo<-bs_sub %>% group_by(comm, dtype) %>% summarize(resi=n())
 
 blist<-future_map(1:6, function(crow){
-    assign(paste0("bs", crow),bs_short %>% filter(comm==combo[crow, "comm"]& dtype==combo[crow, "dtype"]))
+    assign(paste0("bs", crow), bs_sub %>% filter(comm==combo[crow, "comm"]& dtype==combo[crow, "dtype"]))
 })
 
 rm(bs_short)
 loess_fun<- function(x){
     loess(div~tc, data=x)}
 
-loess_res<-map(blist, function(obj){
-    loess_fun(obj)
+loess_res<-future_map(1:6, function(crow){
+    my_x<-bs_sub %>% filter(comm==pull(combo[crow, "comm"])& dtype==pull(combo[crow, "dtype"]))
+    print(head(my_x))    
+    loess_fun(my_x)
 })
     
-
-
+str(loess_res)
